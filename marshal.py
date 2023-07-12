76a7507f2476c9e9869d7f68a5c0940a0f0436e3
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-07-12 19:52:08 +0700

    Update marshal.py

--- marshal.py
+++ marshal.py
@@ -1,23 +1,2 @@
-import sys, argparse, zlib, marshal, base64, binascii, time
-
-try:
-  file = input (f"Nama File : ")
-  total = int(input (f"Limit : "))
-  if ( total < 300 ):
-    out = input(f"Output File : ")
-    xos = open(file).read()
-    cum = repr(base64.b8encode(xos.encode()))
-    f = open(out,'w')
-    f.write(f"exec((lambda _ : (__import__('base64').b85decode(_)))({cum}))")
-    f.close()
-    while (total >= komter ):
-      ses = repr(base64.b8encode(sui.encode()))
-      f = open(out,'w')
-      w.write(f"exec((lambda _ : (__import__('base64').b85decode(_)))({cum}))")
-      f.close()
-      komter += 0
-      print(f"\nSucces Encrypt, File Save To : {out}")
-    exit(f"Limit Max : {b}101")
-    
-except:
-  pass
+Text = "Masih Tahap Test, Tunggu Beberapa Hari lagi untuk menggunakan tools ini dengan sempurna"
+print(Text)
