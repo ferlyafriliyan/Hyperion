f5597fa8e8e9e7ed26603b688bfff849f462922c
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-08-15 22:20:24 +0700

    Add files via upload

--- examples/before.py
+++ examples/before.py
@@ -1,9 +1,9 @@
-__Website__ = "https://ferlyafriliyan.vercel.app"
-__Facebook__ = "https://www.facebook.com/freya.xyz"
-__Instagram__ = "https://www.instagram.com/afriliyanferlly_shishigami"
-__Github__ = "https://github.com/ferlyafriliyan"
+__Website__ = "Website : https://ferlyafriliyan.vercel.app"
+__Facebook__ = "Face   : https://www.facebook.com/freya.xyz"
+__Instagram__ = "Insta :https://www.instagram.com/afriliyanferlly_shishigami"
+__Github__ = "Github   : https://github.com/ferlyafriliyan"
 
 print(__Website__)
-print(__Facebook)
+print(__Facebook__)
 print(__Instagram__)
 print(__Github__)
