fea0c337c88efeb16f5ee24838c5972737bcb5e6
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-07 22:09:55 +0700

    Add files via upload

--- examples/before.py
+++ examples/before.py
@@ -1,9 +1,16 @@
-__Website__ = "Website : https://ferlyafriliyan.vercel.app"
-__Facebook__ = "Face   : https://www.facebook.com/freya.xyz"
-__Instagram__ = "Insta :https://www.instagram.com/afriliyanferlly_shishigami"
-__Github__ = "Github   : https://github.com/ferlyafriliyan"
+import os, sys
 
-print(__Website__)
-print(__Facebook__)
-print(__Instagram__)
-print(__Github__)
+#--> Clear Terminal
+def clear():
+    if "linux" in sys.platform.lower():os.system('clear')
+    elif "win" in sys.platform.lower():os.system('cls')
+
+def hello():
+    myname = ", I am Ferly Afriliyan ! "
+    project = "and I just finished creating a python project to Obfuscate Python3 files which is quite difficult to Reverse"
+    print("Hello world", myname)
+    print(project)
+    
+if __name__ == '__main__':
+    clear();hello()
+    
